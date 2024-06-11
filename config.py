ae28e7c7600bb787f9f62a305ecfebf9c2658285
MridulME <155382503+MridulME@users.noreply.github.com>
2024-06-11 17:21:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 # My-SQL connection Details
-db_uri_conf = 'mysql+pymysql://infi:infi@127.0.0.1:3306/book_system'
+db_uri_conf = 'mysql+pymysql://infi:infi@127.0.0.1:3306/br_system'
 
 # MySql Table Details
 book_data_for_reader = 'book'
